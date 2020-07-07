a6b20efcebef3ecfa977a4c3b98f18addea3d06d
Vovcnik <zaharov.v.a@gmail.com git config --global user.email zaharov.v.a@gmail.com>
2020-07-07 06:13:16 +0300

    added slaider

--- app/js/main.js
+++ app/js/main.js
@@ -2,17 +2,33 @@ $(function() {
 
     $(".rate-star").rateYo({
         rating: 5,
-        starWidth: "17px",
-        rtl: true
+        starWidth: "15px",
+        readOnly: true,
+        spacing: "5px",
+        ratedFill: "#ffc000"
     });
-
+    $(".rate-stars").rateYo({
+        rating: 4,
+        starWidth: "12px",
+        readOnly: true,
+        spacing: "5px",
+        ratedFill: "#ffc000"
+    });
 
     $('.product__item-inner').slick({
         slidesToScroll: 1,
         arrows: true,
 
     });
+    $('.slaider__followers-inner').slick({
+        slidesToScroll: 3,
+        slidesToShow: 3,
+        arrows: true,
+
+    });
 
     $('input[type="file"], select').styler();
+
+    var mixer = mixitup(".release__products-inner");
 
 });
